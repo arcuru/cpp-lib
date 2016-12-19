2b5a4c085c52b52a47ca099206040731e16024d8
Patrick Jackson <PatrickSJackson@gmail.com>
2016-12-18 21:54:03 -0800

    Pushing non-working lru_cache
    So I can work over the holidays.

---
+++ mylib/lru_cache.h
@@ -0,0 +1,69 @@
+
+/**
+ * Least Recently Used Cache
+ *
+ * Stores key/value pairs, while keeping track of the least recently accessed
+ * key for removal once the size is exceeded.
+ */
+template <class key_type, class value_type> class lru_cache {
+
+  struct ll_node {
+    ll_node *next;
+    ll_node *prev;
+    key_type key;
+    value_type val;
+  };
+
+  std::unordered_map<key_type, ll_node *> hmap; //!< Hash map for fast lookups
+  ll_node *head(nullptr);
+  ll_node *tail(nullptr);
+  std::size_t max_size;
+
+  void touch(ll_node *node) {
+    assert(node != nullptr);
+
+    if (head == node)
+      return;
+
+    node->prev->next = node->next;
+
+    if (tail == node) {
+      tail = node->prev;
+      tail->next = nullptr;
+    } else {
+      node->next->prev = node->prev;
+    }
+
+    node->next = head;
+    node->prev = nullptr;
+    head = node;
+  }
+
+public:
+  explicit lru_cache(std::size_t n) : max_size(n) {}
+
+  value_type &get(const key_type &key) {
+    auto it = hmap.find(key);
+    if (it == hmap.end()) {
+      insert(key); // TODO: this is inefficient
+      it = hmap.find(key);
+    }
+    touch(it->second);
+    return it->second->val;
+  }
+
+  void insert(const key_type &key) {
+    auto &it = hmap[key];
+    if (it == nullptr) {
+      it = new ll_node();
+    }
+    it->next = head;
+    it->prev = nullptr;
+    it->key = key;
+    if (head != nullptr)
+      head->prev = it;
+    head = it;
+    if (tail == nullptr)
+      tail = it;
+  }
+};
